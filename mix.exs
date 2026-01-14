78974b1a494619057f048025523e7b60a578b1b7
preciz <barna.kovacs@tutanota.com>
2026-01-14 23:32:21 +0100

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule JSONSchemaEditor.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/example/json_schema_editor"
-  @version "0.1.0"
+  @version "0.2.0"
 
   def project do
     [
