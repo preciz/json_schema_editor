d86e10f7c3e3221c590552fa52400639d00e08fd
preciz <barna.kovacs@tutanota.com>
2026-02-01 18:37:11 +0100

    chore: bump version to 0.9.8 and update CHANGELOG

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule JSONSchemaEditor.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/preciz/json_schema_editor"
-  @version "0.9.7"
+  @version "0.9.8"
 
   def project do
     [
